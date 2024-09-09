bb5d65d43185963fae9b105c26876be423f7823c
nitecse <hitman12457868@gmail.com>
2024-09-09 22:46:33 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Typography } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timeInterval = useRef(null);
+
+  const handleStart = () => {
+    if (isRunning) return;
+    setIsRunning(true);
+    timeInterval.current = setInterval(() => {
+      setTimer((prev) => prev + 10);
+    }, 10);
+  };
+
+  const handleStop = () => {
+    if (!isRunning) return;
+    setIsRunning(false);
+    clearInterval(timeInterval.current);
+  }
+
+  const handleReset = () => {
+    setIsRunning(false);
+    clearInterval(timeInterval.current);
+    setTimer(0);
+  }
+
+  const formatTime = (timer) => {
+    let mins = Math.floor(timer / 60000).toString().padStart(1, '0');
+    let secs = Math.floor((timer / 1000) % 60).toString().padStart(2, '0');
+    return { mins, secs };
+  }
+
+  const { mins, secs } = formatTime(timer);
+
+  useEffect(() => {
+    return (() => clearInterval(timeInterval.current));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography fontWeight='bold' variant='h1'>Stopwatch</Typography>
+      <Typography variant='h3' sx={{ mt: 5, mb: 5 }}>Time: {mins}:{secs}</Typography>
+      {!isRunning ? <Button onClick={handleStart} variant='contained'>Start</Button> : <Button onClick={handleStop} variant='contained'>Stop</Button>}
+      <Button onClick={handleReset} variant='contained' sx={{ ml: 5, }}>Reset</Button>
     </div>
   );
 }
